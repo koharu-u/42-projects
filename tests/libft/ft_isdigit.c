875c3bcf98f3fbf670eff8b9ef4cd87a8d51745d
koharu <koharudotgit@duck.com>
2026-08-25 21:58:28 +0700

    chore: rename `isnum` to `isdigit`

--- tests/libft/ft_isdigit.c
+++ tests/libft/ft_isdigit.c
@@ -1,35 +1,35 @@
 #include <unistd.h>
 
 // Test library prototype
-int	ft_isnum(char *str);
+int	ft_isdigit(int str);
 
 // Main test
 int	main(void)
 {
-	char	*test_string;
+	int	test_string;
 
-	test_string = "91h072913h";
-	if (ft_isnum(test_string))
+	test_string = 'z';
+	if (!(ft_isdigit(test_string)))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	test_string = "1234567890";
-	if (ft_isnum(test_string))
+	test_string = '0';
+	if (ft_isdigit(test_string))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	test_string = "HDKJAdshdjsad";
-	if (!(ft_isnum(test_string)))
+	test_string = 'A';
+	if (!(ft_isdigit(test_string)))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	test_string = "";
-	if (!(ft_isnum(test_string)))
+	test_string = '$';
+	if (!(ft_isdigit(test_string)))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
-	test_string = "&(^*(!@%#^(*@!%&(@)";
-	if (!(ft_isnum(test_string)))
+	test_string = '*';
+	if (!(ft_isdigit(test_string)))
 		write(1, "OK\n", 3);
 	else
 		write(1, "KO\n", 3);
