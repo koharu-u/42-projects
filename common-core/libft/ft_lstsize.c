1e97b1c4945ec98b846101d04853f204113d49e2
koharu <koharudotgit@duck.com>
2026-09-04 22:19:22 +0700

    feat(libft): implement ft_lstsize

---
+++ common-core/libft/ft_lstsize.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstsize.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpiwan <kpiwan@student.42bangkok.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/04 20:36:42 by kpiwan            #+#    #+#             */
+/*   Updated: 2026/09/04 21:10:32 by kpiwan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+unsigned int	ft_lstsize(t_list *lst)
+{
+	t_list			*ptr;
+	unsigned int	counter;
+
+	counter = 0;
+	ptr = lst;
+	while (ptr->next != NULL)
+	{
+		counter++;
+		ptr = lst->next;
+	}
+	return (counter);
+}
