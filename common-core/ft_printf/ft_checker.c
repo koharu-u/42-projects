bf405cf15b35293db78fae3d3ec601c6be7ad094
koharu <koharudotgit@duck.com>
2026-09-17 22:20:59 +0700

    chore(ft_printf): fix makefile and case handle

--- common-core/ft_printf/ft_checker.c
+++ common-core/ft_printf/ft_checker.c
@@ -6,21 +6,11 @@
 /*   By: kpiwan <kpiwan@student.42bangkok.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 08:39:46 by kpiwan            #+#    #+#             */
-/*   Updated: 2026/09/17 21:54:42 by kpiwan           ###   ########.fr       */
+/*   Updated: 2026/09/17 22:20:25 by kpiwan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// -1 is UNKNOWN/ERROR
-//  0 is INT/DECIMAL
-//  1 is SINGLE CHAR
-//  2 is STRING
-//  3 is POINTER
-//  4 is UNSIGNED DECIMAL
-//  5 is HEXADECIMAL LOWER
-//  6 is HEXADECIMAL UPPER
-//  7 is PERCENT SIGN
 
 int	ft_checker(const char *format, size_t position)
 {
